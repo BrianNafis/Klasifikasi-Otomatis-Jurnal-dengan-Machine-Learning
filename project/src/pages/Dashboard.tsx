3fb68dca68e82ab3bc48288cb9e34dcdef66cb2d
BrianNafis <mbrnfs22@gmail.com>
2026-04-15 00:20:26 +0700

    initial commit

--- project/src/pages/Dashboard.tsx
+++ project/src/pages/Dashboard.tsx
@@ -10,7 +10,7 @@ const Dashboard: React.FC = () => {
     {
       icon: DocumentTextIcon,
       label: 'Total Jurnal',
-      value: '1,247',
+      value: '1,248',
       change: '+12%',
       color: 'text-blue-600',
       bgColor: 'bg-blue-50'
